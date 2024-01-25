428dc8d898a7478da7be7f5c5845c35c8bb4f894
nuca23 <125753427+nuca23@users.noreply.github.com>
2024-01-26 00:46:09 +0400

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./header";
+import Header from "./header";
+class App extends React.Component {
+    render(){
+        return (
+         <header></header>
+        )
+    }
+}
+var root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App></App>);
